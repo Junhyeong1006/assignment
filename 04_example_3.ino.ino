350ebe5842ec5eccaad16b5b7bd5ce34316478ec
Junhyeong1006 <105621923+Junhyeong1006@users.noreply.github.com>
2022-09-20 12:55:13 +0900

    04_example_3.ino upload

---
+++ 04_example_3.ino.ino
@@ -0,0 +1,29 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+
+unsigned int toggle_state(unsigned int toggle) {
+    return !toggle;
+  }
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) {
+    
+  }
+  Serial.println("Hello World!");
+  count = 0;
+  toggle = 0;
+  digitalWrite(PIN_LED, toggle);
+  
+}
+
+void loop() {
+  Serial.println(++count);
+  toggle = toggle_state(toggle);
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+
+  
+
+}
