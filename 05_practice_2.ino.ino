ce663179f63d37b9691bc64c820d535a3b445344
Junhyeong1006 <105621923+Junhyeong1006@users.noreply.github.com>
2022-09-22 09:29:17 +0900

    Add files via upload

---
+++ 05_practice_2.ino.ino
@@ -0,0 +1,19 @@
+
+void setup() {
+  pinMode(7, OUTPUT);
+  digitalWrite(7, 0);
+  delay(1000);
+  
+  for (int i =0; i < 5; i++) {
+    
+    digitalWrite(7, 1);
+    delay(100);
+    digitalWrite(7, 0);
+    delay(100);
+
+  }
+}
+
+void loop() {
+  digitalWrite(7, 1);
+}
